185150a1169ad342ca1e877cdd8ce4ac0f8cf552
KikO <145018359+KiKo552@users.noreply.github.com>
2024-12-25 23:02:40 +0100

    The game is playable now! (but not really)
    Some functionality like replay-ability and point counter are not in yet but you can still play against a bot :P

---
+++ TikTakToe/TikTakToe/TikTakToe/Settings.h
@@ -0,0 +1,18 @@
+#ifndef SETTINGS_H
+    #define SETTINGS_H
+    /*Window customization*/
+    // Window dimensions [pixels]
+    #define WINDOW_WIDTH    500
+    #define WINDOW_HEIGHT   500
+
+    // Window properties :P
+    #define WINDOW_TITLE    "TikTakToe"
+
+    // Window styles 😎
+    #define WINDOW_STYLE    WS_OVERLAPPEDWINDOW
+    #define WINDOW_EX_STYLE WS_EX_CLIENTEDGE
+
+    /*Backend customization*/
+    #define PLAYER  1
+    #define BOT     2
+#endif
